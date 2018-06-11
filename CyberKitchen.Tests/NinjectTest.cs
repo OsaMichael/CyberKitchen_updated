5402f26923a0ca23530c2603471c6e65026f7dce
evan mikeosa <evanmikeosa@yahoo.com>
2018-06-11 12:36:54 +0100

    initial commit

---
+++ CyberKitchen.Tests/NinjectTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+using Cyber_Kitchen.Interface;
+using System.Reflection;
+
+namespace CyberKitchen.Tests
+{
+    [TestClass]
+    public class NinjectTest
+    {
+        [TestMethod]
+        public void TestNinjectBindings()
+        {
+            //Create Kernel
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.Load("Cyber_Kitchen"));
+            kernel.Get<IRestaurantManager>();
+        }
+    }
+}
